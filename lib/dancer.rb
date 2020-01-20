a24467678b6c0169f412e7bd85b76014e5631e75
Asija Watson <asija.w@yahoo.com>
2020-01-20 17:26:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,13 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Kid
-  include Dance
-  extend MetaDancing
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
